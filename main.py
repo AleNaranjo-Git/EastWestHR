fddf2ee392106f9f076dc1c4242204e0a8d9e530
Alejandro Naranjo <aleunaranjo@gmail.com>
2025-06-09 19:19:42 -0600

    Date and Loggin Utils

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from dotenv import load_dotenv
+import os
+from utils.logging_config import setup_logging
+
+load_dotenv()  # Esto carga las variables del .env
+
+# Set log level based on environment
+env = os.getenv("APP_ENV", "development")
+if env == "production":
+    setup_logging(default_level="INFO", file_level="INFO")
+else:
+    setup_logging(default_level="DEBUG", file_level="INFO")
+
 from PySide6.QtWidgets import QApplication
 from ui.windows.login_window import LoginWindow
 import sys
